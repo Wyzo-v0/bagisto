ba333775aaabdc505f537bec432f035301c76bdc
safarovitch_jr <r.safarovitch@gmail.com>
2025-02-08 17:26:14 +0500

    Update Installer.php

--- packages/Webkul/Installer/src/Listeners/Installer.php
+++ packages/Webkul/Installer/src/Listeners/Installer.php
@@ -12,7 +12,7 @@ class Installer
      *
      * @var string
      */
-    protected const API_ENDPOINT = 'https://updates.bagisto.com/api/updates';
+    protected const API_ENDPOINT = 'https://updates.wyzo.ahop/api/updates';
 
     /**
      * Create a new listener instance.
